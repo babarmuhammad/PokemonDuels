07dbd0e8f1a9e20e89baf56f4d96d6fcf268ab0f
JEANMONNET\babar_muhammad_anas <babarh174@gmail.com>
2021-12-17 13:55:49 +0100

    Aggiunto le classi e il file elenco mosse

---
+++ PokemonDuels/src/pokemonduels/Comunicazione/Server.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokemonduels.Comunicazione;
+
+/**
+ *
+ * @author babar_muhammad_anas
+ */
+public class Server {
+    
+}
--- PokemonDuels/src/pokemonduels/Grafica/Grafica.java
+++ PokemonDuels/src/pokemonduels/Grafica/Grafica.java
@@ -3,18 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pokemonduels;
+package pokemonduels.Grafica;
 
 /**
  *
  * @author babar_muhammad_anas
  */
-public class NewJFrame extends javax.swing.JFrame {
+public class Grafica extends javax.swing.JFrame {
 
     /**
-     * Creates new form NewJFrame
+     * Creates new form Grafica
      */
-    public NewJFrame() {
+    public Grafica() {
         initComponents();
     }
 
@@ -60,20 +60,20 @@ public class NewJFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Grafica.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Grafica.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Grafica.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Grafica.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewJFrame().setVisible(true);
+                new Grafica().setVisible(true);
             }
         });
     }
