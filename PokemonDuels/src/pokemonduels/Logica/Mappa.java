5d44c0991fab39b65d7eb77733b485df080015e1
LAPTOP-LHAVCF12\babar <babarh174@gmail.com>
2021-12-27 19:19:14 +0100

    Classe mappa

--- PokemonDuels/src/pokemonduels/Logica/Mappa.java
+++ PokemonDuels/src/pokemonduels/Logica/Mappa.java
@@ -5,10 +5,32 @@
  */
 package pokemonduels.Logica;
 
+import java.awt.Canvas;
+import java.awt.Rectangle;
+import java.util.List;
+import java.util.Timer;
+import pokemonduels.Grafica.Grafica;
+
 /**
  *
  * @author babar_muhammad_anas
  */
 public class Mappa {
-    
+    public List<Nodo> mappa;
+        Integer Destinazione;
+        boolean turno;
+        List<Integer> startPosizionamento;
+        Canvas myCanvas;
+        Grafica m;
+        int partiX, partiY;
+        List<Rectangle> Grafica;
+        int PartiPerMano;
+        int PedineMappa,PedineMano;
+        Nodo Selezionato;
+        int Nturno,Tvinti;
+        public Giocatore io, altro;
+        //public Ruota r;
+        //GestioneRuota gr;
+        public int mioAttacco;
+        Timer t;
 }
