7b718b819a486a00e272887ff809352812a3b022
ludovicored03 <ludovicored03@gmail.com>
2021-12-27 17:59:54 +0100

    classi pokemon e mosse

--- PokemonDuels/src/pokemonduels/Logica/Mossa.java
+++ PokemonDuels/src/pokemonduels/Logica/Mossa.java
@@ -10,5 +10,32 @@ package pokemonduels.Logica;
  * @author babar_muhammad_anas
  */
 public class Mossa {
+
+   
+  
+    public Integer percentuale, danno,id;
+    public String descrizione,nome,colore;
     
+     public Mossa(Integer id, Integer percentuale, Integer danno, String descrizione, String nome) {
+        this.id = id;
+        this.percentuale = percentuale;
+        this.danno = danno;
+        this.descrizione = descrizione;
+        this.nome = nome;
+        this.colore=colore;
+    }
+     public void fromCSV(String csv){
+         String[] campo=csv.split(";");
+         id=Integer.parseInt(campo[0]);
+         nome=campo[1];
+         colore=campo[2];
+         descrizione=campo[3];
+         danno=Integer.parseInt(campo[4]);
+         percentuale=Integer.parseInt(campo[5]);
+     }
+     
+    @Override
+     public String toString(){
+     return nome+";"+percentuale+";"+danno;
+     }
 }
--- PokemonDuels/src/pokemonduels/Logica/Pokemon.java
+++ PokemonDuels/src/pokemonduels/Logica/Pokemon.java
@@ -5,10 +5,31 @@
  */
 package pokemonduels.Logica;
 
+import java.util.List;
+
 /**
  *
  * @author babar_muhammad_anas
  */
 public class Pokemon {
-    
+   public  Integer id;
+   public String nome,urlImmagine;
+   public Integer Salti;
+   public List<Integer> Mosse;
+   private Integer NMosse;
+   
+   
+   public void fromCsv(String csv){
+    String[]campo=csv.split(";");
+    id=Integer.parseInt(campo[0]);
+    nome=campo[1];
+    Salti=Integer.parseInt(campo[2]);
+    for(Integer i=0;i<Integer.parseInt(campo[3]);i++)
+        Mosse.add(NMosse++);
+    urlImmagine=id+"png";
+        
 }
+}
+
+
+
