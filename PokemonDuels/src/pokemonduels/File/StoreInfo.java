6bc0865cf3b8555b15edc08ff5b2f9e82ba1f87a
LAPTOP-LHAVCF12\babar <babarh174@gmail.com>
2022-01-05 18:14:28 +0100

    Finito Classe storeInfo

--- PokemonDuels/src/pokemonduels/File/StoreInfo.java
+++ PokemonDuels/src/pokemonduels/File/StoreInfo.java
@@ -5,10 +5,49 @@
  */
 package pokemonduels.File;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import pokemonduels.Logica.Mossa;
+import pokemonduels.Logica.Pokemon;
+
 /**
  *
  * @author babar_muhammad_anas
  */
 public class StoreInfo {
+    public List<String> listMosse = new ArrayList<String>();
+    public List<String> listPokemon = new ArrayList<String>();
     
+    public void creaListaMosse() throws FileNotFoundException{
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("Mosse.csv"));
+            String line = reader.readLine();
+            
+            while(line!=null) {
+                listMosse.add(line);
+                line = reader.readLine();
+            }       
+            } catch (IOException ex) {
+            Logger.getLogger(StoreInfo.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+    public void creaListaPokemon() throws FileNotFoundException{
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("Pokemon.csv"));
+            String line = reader.readLine();
+            
+            while(line!=null) {
+                listPokemon.add(line);
+                line = reader.readLine();
+            }       
+            } catch (IOException ex) {
+            Logger.getLogger(StoreInfo.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
 }
